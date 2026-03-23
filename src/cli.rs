f1416ee0c5804475707b187c6ea0293c8ea7aa61
sunsetlover36 <sunsetloverrr36@gmail.com>
2026-03-23 16:05:34 +0300

    Version bump

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(name = "rock")]
 #[command(author = "Luther Blissett")]
-#[command(version = "0.1.6")]
+#[command(version = "0.2.0")]
 #[command(about = "ROCK game engine", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
